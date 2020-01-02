dcb4cc9f5016d8cb0e894b0d9293737c48b2eb32
@dklymkovskyi <dmytro.klymkovskyi@engineering.ait.com>
2020-01-02 17:39:23 +0200

    route

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -29,6 +29,7 @@ class BurgerBuilder extends Component {
   };
 
   componentDidMount() {
+		console.log(this.props)
     axios
       .get("https://mycrm-187a2.firebaseio.com/ingredients.json")
       .then(resp => {
@@ -86,26 +87,27 @@ class BurgerBuilder extends Component {
   };
   purchaseContinueHandler = () => {
     //alert('You continue!');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Test",
-        address: {
-          street: "Teststreet 1"
-        },
-        email: "test@test.com"
-      }
-    };
-    axios
-      .post("/orders.json", order)
-      .then(resp => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: "Test",
+    //     address: {
+    //       street: "Teststreet 1"
+    //     },
+    //     email: "test@test.com"
+    //   }
+    // };
+    // axios
+    //   .post("/orders.json", order)
+    //   .then(resp => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch(error => {
+    //     this.setState({ loading: false, purchasing: false });
+		//   });
+		this.props.history.push('/checkout')
   };
   render() {
     const disabledInfo = {
